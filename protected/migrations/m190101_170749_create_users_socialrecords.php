18c513f4db0df400353ba82e143650cd8048b8d3
Andriy Semenets <semteacher@gmail.com>
2018-12-30 21:46:03 +0200

    gsuite cron - create details storage table

---
+++ protected/migrations/m190101_170749_create_users_socialrecords.php
@@ -0,0 +1,33 @@
+<?php
+
+class m190101_170749_create_users_socialrecords extends CDbMigration
+{
+	public function safeUp()
+	{
+		$sql = <<<SQL
+		/*Store users social services (e.g. G Suite Directory) profile extra data*/
+        CREATE TABLE USERS_SOCIALRECORDS (
+            ID  INTE PRIMARY KEY /*Почта для подвреждения*/,
+            USERID  INTE /*USER ID*/,
+            USERTYPE SMALLINT DEFAULT NULL /*U5 from users*/,
+            PERSONID INTEGER DEFAULT NULL /*U6 from useers*/,
+            SERVICE VAR20 /*social service name*/,
+            SERVICEID VARCHAR(50) DEFAULT NULL, 
+            CREATED DAT,
+            UPDATED DAT
+        );
+SQL;
+		$this->execute($sql);
+
+        $sql = <<<SQL
+        ALTER TABLE USERS_SOCIALRECORDS ADD CONSTRAINT FK_USERS_SOCIALRECORDS_1 FOREIGN KEY (USERID) REFERENCES USERS (U1) ON DELETE CASCADE ON UPDATE CASCADE;
+SQL;
+        $this->execute($sql);
+	}
+
+	public function safeDown()
+	{
+		echo "m190101_170749_create_users_socialrecords does not support migration down.\\n";
+		return false;
+	}
+}
